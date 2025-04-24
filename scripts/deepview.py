3d6dc22fb788ad12f35fc468bbbeef2588ddf689
aishwariyachakraborty <aishwariya.chakraborty@gmail.com>
2025-04-24 17:58:25 +0530

    suggested changes
    Signed-off-by: aishwariyachakraborty <aishwariya.chakraborty@gmail.com>
    
    
    Signed-off-by: IBM DeepView Contributors <contributor@users.noreply.github.com>

--- scripts/deepview.py
+++ scripts/deepview.py
@@ -2,6 +2,10 @@ import os
 import subprocess
 import sys
 import argparse
+import shutil
+import torch_sendnn
+
+
 
 def modify_file(original_file, insert_file, x, y):
     with open(original_file, 'r') as f:
@@ -18,71 +22,63 @@ parser = argparse.ArgumentParser(
 )
 
 parser.add_argument(
-    "--sendnn_path",
-    type=str,
-    default="torch_sendnn",
-    help="Path to torch_sendnn package folder",
+    '--model_type', 
+    choices=['fms' 'hf'],
+    required=True, 
+    default='fms',
+    help='The type of model you want to debug - fms or hf.'
 )
 
 parser.add_argument(
-    '--show_stack_trace',
+    '--model', 
+    required=True, 
+    help='Model name in HF format or model path'
+)
+
+parser.add_argument(
+    '--show_details',
     action='store_true',
-    help="Print stack trace with unsupported op."
+    help="Print stack trace and other details with unsupported op."
 )
-
-subparsers = parser.add_subparsers(
-    dest='model_type', 
-    required=True, 
-    help='The type of model you want to debug.'
-)
-
-parser_hf = subparsers.add_parser('hf', help='For running HF models directly')
-parser_hf.add_argument(
-    '--model_name', 
-    required=True, 
-    help='Model name in HF format'
-)
-
-parser_fms = subparsers.add_parser('fms', help='For running FMS models')
-parser_fms.add_argument(
-    '--model_path', 
-    required=True, 
-    help='Path to the FMS model'
-)
-
 
 
 args = parser.parse_args()
 
+
+## USE AST
 convert_unknown_lineno = (543,556)
-original_file_path = os.path.join(args.sendnn_path, "torch_sendnn", "backends.py")
+sendnn_folder = os.path.dirname(torch_sendnn.__file__)
+dst_folder = os.path.join(os.getcwd(), os.path.basename(sendnn_folder))
+shutil.copytree(sendnn_folder, dst_folder)
+
+original_file_path = os.path.join(dst_folder, 'torch_sendnn', 'backends.py')
 insert_file_path = '../core/function_modifications/convert_unknown_func.txt'
-if args.show_stack_trace:
+if args.show_details:
     insert_file_path = '../core/function_modifications/convert_unknown_func_with_stack_trace.txt'
 modify_file(original_file_path, insert_file_path, convert_unknown_lineno[0], convert_unknown_lineno[1])
 
-
+## Add more information (layers etc)
 
 # lazy_handles_lineno = (1998,2002)
 
 
 # Reinstall torch_sendnn library 
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-e', '.'], cwd=args.sendnn_path)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-e', '.'], cwd=dst_folder)
 # Append torch_sendnn to python path
-sys.path.insert(0, args.sendnn_path)
+sys.path.insert(0, dst_folder)
 
 
 if args.model_type == 'hf':
     command = [
         'python3', '../core/inference_hf.py',
-        '--model_name', args.model_name,
+        '--model_name', args.model,
     ]
 elif args.model_type == 'fms':
     command = [
         'python3', '../core/inference_fms.py',
         '--architecture', 'hf_pretrained',
-        '--model_path', args.model_path,
-        '--tokenizer', args.model_path, 
+        '--model_path', args.model,
+        '--tokenizer', args.model, 
         '--device_type', 'aiu',
         '--unfuse_weights', 
         '--compile', 
@@ -95,11 +91,29 @@ elif args.model_type == 'fms':
     ]
 
 
-  
-with open('model_output.txt', 'w') as f:
-    subprocess.run(
+with open("model_output.txt", "w") as f:
+    process = subprocess.Popen(
         command,
-        stdout=f,    # ✅ stdout redirected to file
-        stderr=subprocess.STDOUT   # ✅ stderr merged into stdout
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True
     )
+    for line in process.stdout:
+        print(line, end='')  # ✅ print to terminal
+        f.write(line)        # ✅ write to file
+    process.wait()
 
+
+input_file = "model_output.txt"
+output_file = "debug_tool_output.txt"
+
+with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+    for line in infile:
+        if line.startswith("DEBUG TOOL"):
+            outfile.write(line)
+
+## If the code breaks, still should output 
+## Show output in terminal as well as save in file
+## pipe debug tool output to another file
+
+## Handle case: All ops are supported 
