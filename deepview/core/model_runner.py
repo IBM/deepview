6b1c826a862541b47e768823e52a261f09c83a2c
aishwariyachakraborty <aishwariya.chakraborty@gmail.com>
2025-07-03 20:35:20 +0530

    trial
    Signed-off-by: aishwariyachakraborty <aishwariya.chakraborty@gmail.com>
    
    
    Signed-off-by: IBM DeepView Contributors <contributor@users.noreply.github.com>

--- deepview/core/model_runner.py
+++ deepview/core/model_runner.py
@@ -88,8 +88,17 @@ def run_io_dumping_mode(aiu_model_handler, deepview_mode, model_path, model_type
     aiu_model_handler.get_layer_io()
     aiu_model_handler.remove_forward_hooks()
     aiu_model_handler.clear_layer_io()
+
+    new_aiu_model_handler = ModelHandler(
+                model_type=model_type,
+                model_path=model_path,
+                device='aiu',
+                prompt="What is the capital of Egypt?",
+            )
+    new_aiu_model_handler.layer_inputs = aiu_model_handler.layer_inputs
+    del aiu_model_handler
     aiu_layer_io = generate_individual_layer_output(
-        aiu_model_handler,
+        new_aiu_model_handler,
         model_path,
         model_type,
         'aiu',
