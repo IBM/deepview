9313d4d4b180650b4990be1d40d0d4a2e71062e8
Sujit Mahto <sujitmahto@Sujits-MacBook-Pro.local>
2025-09-18 11:46:19 +0530

    adding patch to fix for backends failure in warmup
    Signed-off-by: IBM DeepView Contributors <contributor@users.noreply.github.com>

--- patch.sh
+++ patch.sh
@@ -7,5 +7,6 @@
 patch -p 1 $DRYRUN -d $SEN_PROJECT_SRC/deeptools < ./issue3499.patch
 patch -p 1 $DRYRUN -d $SEN_PROJECT_SRC/deeptools < ./issue3633.patch
 patch -p 1 $DRYRUN -d $SEN_PROJECT_SRC/deeptools < ./pr3721.patch
-patch -p 1 $DRYRUN -d $SEN_PROJECT_SRC/torch_sendnn < ./pr262.patch
+#patch -p 1 $DRYRUN -d $SEN_PROJECT_SRC/torch_sendnn < ./pr262.patch
+patch -p 1 $DRYRUN -d $SEN_PROJECT_SRC/torch_sendnn < ./backends_fix.patch
 ) 2>&1 | tee ${0%.*}.log
