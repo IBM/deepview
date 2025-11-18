1771b6b7821385dcee80c202bf332d23666e23e4
Pablo Gonzalez <contributor@users.noreply.github.com>
2025-11-18 14:35:19 +0100

    fix tox ruff checks
    Signed-off-by: Pablo Gonzalez <pablo.gonzalez3@ibm.com>
    
    
    Signed-off-by: IBM DeepView Contributors <contributor@users.noreply.github.com>

--- tests/deepview_model_runner.py
+++ tests/deepview_model_runner.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import argparse
 import subprocess
 
-
 TEST_MODELS: list[str] = [
     "ibm-granite/granite-3.2-2b-instruct",
     "ibm-granite/granite-3.2-8b-instruct",
